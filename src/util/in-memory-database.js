2aa4264c95126764ee02223e45075f5e0cf07362
Waros <wyayla@gmail.com>
2023-05-08 20:19:28 +0200

    Refactoring finished

---
+++ src/util/in-memory-database.js
@@ -0,0 +1,18 @@
+module.exports = {
+    users: [
+      {
+        id: 0,
+        firstName: 'Hendrik',
+        lastName: 'van Dam',
+        emailAdress: 'hvd@server.nl',
+        password: 'PythonisbeterdanJava'
+      },
+      {
+        id: 1,
+        firstName: 'Marieke',
+        lastName: 'Jansen',
+        emailAdress: 'm@server.nl',
+        password: 'MaarC#isdebeste'
+      }
+    ],
+}
