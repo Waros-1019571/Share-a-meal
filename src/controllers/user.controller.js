3247a4ce6b259161359eee8b8c05f3b151727ce9
Waros <wyayla@gmail.com>
2023-05-08 21:02:06 +0200

    Input validation finished

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,18 +8,31 @@ const postUser = (req, res) => {
     logger.debug('POST /api/user aangeroepen met body: ' + JSON.stringify(req.body))
     const user = req.body
     try {
+        assert(typeof user.emailAdress === 'string', 'Het e-mailadres moet een string zijn')
+        assert(user.emailAdress.length !== 0, 'Het e-mailadres moet minimaal een karakter lang zijn')
+        assert(typeof user.firstName === 'string', 'De voornaam moet een string zijn')
+        assert(user.firstName.length !== 0, 'De voornaam moet minimaal een karakter lang zijn')
+        assert(typeof user.lastName === 'string', 'De achternaam moet een string zijn')
+        assert(user.lastName.length !== 0, 'De achternaam moet minimaal een karakter lang zijn')
+        assert(typeof user.password === 'string', 'Het wachtwoord moet een string zijn')
+        assert(user.password.length !== 0, 'Het wachtwoord moet minimaal een karakter lang zijn')
         for (let i = 0; i < database.users.length; i++) {
-            assert(typeof user.emailAdress === 'string', 'Het e-mailadres moet een string zijn')
-            assert(user.emailAdress !== database.users[i].emailAdress, 'Het e-mailadres is al in gebruik!')
+            assert(user.emailAdress !== database.users[i].emailAdress, 'Het e-mailadres is al in gebruik!')    
         }
-        user.id = index++
-        database.users.push(user)
-        logger.info('User ' + user.id + ' toegevoegd')
-        res.status(201).send(user)
     } catch (err) {
         logger.error(err.message)
         res.status(400).end(err.message)
+        return
     }
+    database.users.push({
+        id: index++,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAdress: user.emailAdress,
+        password: user.password,
+    })
+    logger.info('User ' + user.id + ' toegevoegd')
+    res.status(201).send(user)
 }
 
 //202
@@ -37,9 +50,17 @@ const getProfile = (req, res) => {
 //204
 const getUser = (req, res) => {
     logger.debug('GET /api/user/:userId aangeroepen met userId: ' + req.params.userId)
-    const userId = req.params.userId
+    const userId = Number(req.params.userId)
+    try {
+        assert(!isNaN(userId), 'Het Id moet een nummer zijn')
+        assert(userId >= 0, 'Het id moet minimaal \'0\' zijn')
+    } catch (err) {
+        logger.error(err.message)
+        res.status(400).end(err.message)
+        return
+    }
     for (let i = 0; i < database.users.length; i++) {
-        if (userId === database.users[i].id.toString()) {
+        if (userId === database.users[i].id) {
             res.send(database.users[i])
             return
         }
@@ -51,20 +72,37 @@ const getUser = (req, res) => {
 //205
 const putUser = (req, res) => {
     logger.debug('PUT /api/user/:userId aangeroepen met userId: ' + req.params.userId + ' en body: ' + JSON.stringify(req.body))
-    const userId = req.params.userId
+    const userId = Number(req.params.userId)
+    const user = req.body
+    try {
+        assert(!isNaN(userId), 'Het Id moet een nummer zijn')
+        assert(userId >= 0, 'Het id moet minimaal \'0\' zijn')
+        assert(user.emailAdress == null || typeof user.emailAdress === 'string', 'Het e-mailadres moet een string zijn')
+        assert(user.emailAdress == null || user.emailAdress.length !== 0, 'Het e-mailadres moet minimaal een karakter lang zijn')
+        assert(user.firstName == null   || typeof user.firstName === 'string', 'De voornaam moet een string zijn')
+        assert(user.firstName == null   || user.firstName.length !== 0, 'De voornaam moet minimaal een karakter lang zijn')
+        assert(user.lastName == null    || typeof user.lastName === 'string', 'De achternaam moet een string zijn')
+        assert(user.lastName == null    || user.lastName.length !== 0, 'De achternaam moet minimaal een karakter lang zijn')
+        assert(user.password == null    || typeof user.password === 'string', 'Het wachtwoord moet een string zijn')
+        assert(user.password == null    || user.password.length !== 0, 'Het wachtwoord moet minimaal een karakter lang zijn')
+    } catch (err) {
+        logger.error(err.message)
+        res.status(400).end(err.message)
+        return
+    }
     for (let i = 0; i < database.users.length; i++) {
-        if (userId === database.users[i].id.toString()) {
+        if (userId === database.users[i].id) {
             if (req.body.firstName != null) {
-                database.users[i].firstName = req.body.firstName
+                database.users[i].firstName = user.firstName
             }
             if (req.body.lastName != null) {
-                database.users[i].lastName = req.body.lastName
+                database.users[i].lastName = user.lastName
             }
             if (req.body.emailAdress != null) {
-                database.users[i].emailAdress = req.body.emailAdress
+                database.users[i].emailAdress = user.emailAdress
             }
             if (req.body.password != null) {
-                database.users[i].password = req.body.password
+                database.users[i].password = user.password
             }
             logger.info('User ' + userId + ' is aangepast')
             res.send(database.users[i])
@@ -78,10 +116,18 @@ const putUser = (req, res) => {
 //206
 const deleteUser = (req, res) => {
     logger.debug('DELETE /api/user/:userId aangeroepen met userId: ' + req.params.userId)
-    const userId = req.params.userId
+    const userId = Number(req.params.userId)
+    try {
+        assert(!isNaN(userId), 'Het Id moet een nummer zijn')
+        assert(userId >= 0, 'Het id moet minimaal \'0\' zijn')
+    } catch (err) {
+        logger.error(err.message)
+        res.status(400).end(err.message)
+        return
+    }
     let deletePosition = null
     for (let i = 0; i < database.users.length; i++) {
-        if (userId === database.users[i].id.toString()) {
+        if (userId === database.users[i].id) {
             deletePosition = i
             break
         }
